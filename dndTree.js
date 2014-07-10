f8a22cb918674f55550020e6140b7c1c42469c4c
anniepyim <anniepyim@gmail.com>
2014-07-10 17:26:35 +0100

    Update dndTree.js

--- dndTree.js
+++ dndTree.js
@@ -10,7 +10,7 @@ treeJSON = d3.json("test.json", function(error, treeData) {
     var panBoundary = 20; // Within 20px from edges will pan when dragging.
     // Misc. variables
     var i = 0;
-    var duration = 3000;
+    var duration = 200;
     var root;
     
     // size of the diagram
