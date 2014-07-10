04701cf90025677e83e7c6f71e057eecdbcd79fc
anniepyim <anniepyim@gmail.com>
2014-07-10 12:51:53 +0100

    Update areachart.js

--- areachart.js
+++ areachart.js
@@ -91,7 +91,7 @@ d3.csv("Nanog-avg.csv", function(error, data) {
 });
 
 function brushed() {
-  x.domain(brush.empty() ? x2.domain() : brush.extent());
+  x.domain(brush.empty() ? x2.domain() : y2.domain());//brush.extent());
   focus.select(".area").attr("d", area);
   focus.select(".x.axis").call(xAxis);
 }
