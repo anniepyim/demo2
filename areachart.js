11a9a0138903bdd1f59d03303619b5f8d4923e99
anniepyim <anniepyim@gmail.com>
2014-07-11 12:55:56 +0100

    Update areachart.js

--- areachart.js
+++ areachart.js
@@ -90,7 +90,7 @@ d3.csv("Nanog-avg.csv", function(error, data) {
       //.classed("hover", true)
       //.attr("stroke", strokecolor)
       //.attr("stroke-width", "0.5px"), 
-      tooltip.html(invertedx).style("visibility", "visible");
+      tooltip.html(String.valueOf(invertedx)).style("visibility", "visible");
       
       
 
