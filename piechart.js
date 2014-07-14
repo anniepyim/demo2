b5ee1b04bff1c394433b768e4643b7ed07ae9623
anniepyim <anniepyim@gmail.com>
2014-07-14 15:20:38 +0100

    Update piechart.js

--- piechart.js
+++ piechart.js
@@ -43,6 +43,7 @@ d3.csv("celltypecount.csv", function(error, data) {
            .attr("opacity", "1");
       })
       .on("click", function(d) {
+        alert(d.data.cell);
         var csvString = d.data.cell;
         var a = document.createElement('a');
         a.href     = 'data:attachment/csv,' + csvString;
